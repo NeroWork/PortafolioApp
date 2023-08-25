d3f90ffda6c6fc74918d0ea58c2b1d1cf0bd0fb3
NeroWork <84593907+NeroWork@users.noreply.github.com>
2023-08-25 18:38:54 -0300

    a

--- src/components/main/projects.jsx
+++ src/components/main/projects.jsx
@@ -3,6 +3,7 @@ import SingleProject from "../projects/singleProject";
 const Proyects = () => {
     let definitionText1 = "This project was done to practice dynamic rendering techniques based on the state of the database, with ReactJS. The website simulates a sales page for a local bakery.";
     let definitionText2 = "This is a simple project in which I use ReactJS and Bootstrap to create a portfolio where anyone can see me and my projects";
+    let definitionText3 = "This is where I learned all the basics of non relational data bases, in this case MongoDB; and how to work with Express js. It's not meant to be pretty and it's not, but it helped me learn how to create a functional back end";
 
     return(
         <div className="row forthRow text-center">
@@ -14,6 +15,7 @@ const Proyects = () => {
                 </div>
                 <SingleProject title="Kokebok" homeLink="https://nerowork.github.io/kokebok-react/" labLink="https://gitlab.com/NeroWork/kokebok-react" definition={definitionText1} imgHomeLink="http://drive.google.com/uc?export=view&id=1UUa5glywvUBxuSUMHXJDdciOxv2-jAK2"></SingleProject>
                 <SingleProject title="Portfolio" homeLink="https://nerowork.github.io/PortafolioApp/" labLink="https://gitlab.com/NeroWork/PortafolioApp" definition={definitionText2} imgHomeLink="https://drive.google.com/uc?export=view&id=1_iZ37GqLgoz6jtF2MvTxagSSWodoVpze"></SingleProject>
+                <SingleProject title="Backend" homeLink="https://backend-16d8.onrender.com/session/render" labLink="https://gitlab.com/NeroWork/BACKEND-DEPLOY" definition={definitionText3} imgHomeLink="https://drive.google.com/uc?export=view&id=1213zeUJ4qi3B_xfnNFARoZlqxKRTTHRr"></SingleProject>
             </div>
         </div>
     )
